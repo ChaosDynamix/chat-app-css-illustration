b6396b1a8e22dd5a7b26836ded5a1ef1d4a691ba
john-mirage <chaosdynamix@posteo.net>
2022-09-10 10:39:54 +0200

    feat: remove sass

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-const { defineConfig } = require('vite')
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/chat-app-css-illustration/",
-})
+    resolve: {
+      alias: {
+        '@images': resolve(__dirname, 'src/images'),
+        '@styles': resolve(__dirname, 'src/styles'),
+      },
+    },
+});
