5e97f62005a846bd3e7680499088b21cd0b27e12
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-18 22:06:32 +0200

    fix: remove base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/chat-app-css-illustration/",
     resolve: {
       alias: {
         '@images': resolve(__dirname, 'src/images'),
